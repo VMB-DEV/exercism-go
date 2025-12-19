5a2d9102fc3dcfc64d9bf8ca4eb62022b6194573
VMB-DEV <vkdev7360@gmail.com>
2025-12-19 08:51:03 +0100

    (raindrops.go)

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -1,5 +1,19 @@
 package raindrops
 
-func Convert(number int) string {
-	panic("Please implement the Convert function")
+import "strconv"
+
+func Convert(number int) (ret string) {
+	if number%3 == 0 {
+		ret += "Pling"
+	}
+	if number%5 == 0 {
+		ret += "Plang"
+	}
+	if number%7 == 0 {
+		ret += "Plong"
+	}
+	if len(ret) == 0 {
+		ret = strconv.Itoa(number)
+	}
+	return
 }
