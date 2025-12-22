fd804e1487b99418127e3e9e63fd45abc1cbfdf7
VMB-DEV <vkdev7360@gmail.com>
2025-12-22 09:04:02 +0100

    (hamming.go)

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,5 +1,15 @@
 package hamming
 
-func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+import "errors"
+
+func Distance(a, b string) (d int, err error) {
+	if len(a) != len(b) {
+		return 0, errors.New("strings must be equal")
+	}
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			d++
+		}
+	}
+	return
 }
