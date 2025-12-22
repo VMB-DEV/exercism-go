9f1cd6f33ef49295342b378e6b6555e2dc5502d4
VMB-DEV <vkdev7360@gmail.com>
2025-12-22 09:05:51 +0100

    (hamming.go)

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -6,7 +6,7 @@ func Distance(a, b string) (d int, err error) {
 	if len(a) != len(b) {
 		return 0, errors.New("strings must be equal")
 	}
-	for i := 0; i < len(a); i++ {
+	for i := range a {
 		if a[i] != b[i] {
 			d++
 		}
