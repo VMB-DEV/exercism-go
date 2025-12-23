81c3e294766d13880fd500b478af4820d28cb3c7
VMB-DEV <vkdev7360@gmail.com>
2025-12-23 09:17:55 +0100

    (strain.go) Generics

--- strain/strain.go
+++ strain/strain.go
@@ -1,22 +1,12 @@
 package strain
 
-// Implement the "Keep" and "Discard" function in this file.
-
-// You will need typed parameters (aka "Generics") to solve this exercise.
-// They are not part of the Exercism syllabus yet but you can learn about
-// them here: https://go.dev/tour/generics/1
+func Discard[T any](list []T, predicate func(T) bool) (result []T) {
+	return Keep(list, func(item T) bool { return !predicate(item) })
+}
 
 func Keep[T any](list []T, predicate func(T) bool) (result []T) {
-	return myFilter(list, predicate, false)
-}
-
-func Discard[T any](list []T, predicate func(T) bool) (result []T) {
-	return myFilter(list, predicate, true)
-}
-
-func myFilter[T any](list []T, predicate func(T) bool, invert bool) (result []T) {
 	for _, item := range list {
-		if (predicate(item) && !invert) || (!predicate(item) && invert) {
+		if predicate(item) {
 			result = append(result, item)
 		}
 	}
