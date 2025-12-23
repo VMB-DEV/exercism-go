357019bafcaff376f54165e712688276ad25331f
VMB-DEV <vkdev7360@gmail.com>
2025-12-23 09:15:17 +0100

    (strain.go) Generics

--- strain/strain.go
+++ strain/strain.go
@@ -6,17 +6,17 @@ package strain
 // They are not part of the Exercism syllabus yet but you can learn about
 // them here: https://go.dev/tour/generics/1
 
-func Keep[T any](list []T, filter func(T) bool) (result []T) {
-	return myFilter(list, filter)
+func Keep[T any](list []T, predicate func(T) bool) (result []T) {
+	return myFilter(list, predicate, false)
 }
 
-func Discard[T any](list []T, filter func(T) bool) (result []T) {
-	return myFilter(list, func(item T) bool { return !filter(item) })
+func Discard[T any](list []T, predicate func(T) bool) (result []T) {
+	return myFilter(list, predicate, true)
 }
 
-func myFilter[T any](list []T, filter func(T) bool) (result []T) {
+func myFilter[T any](list []T, predicate func(T) bool, invert bool) (result []T) {
 	for _, item := range list {
-		if filter(item) {
+		if (predicate(item) && !invert) || (!predicate(item) && invert) {
 			result = append(result, item)
 		}
 	}
