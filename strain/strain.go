2690336306cd06cc06c6d5036f3fd5b090352938
VMB-DEV <vkdev7360@gmail.com>
2026-01-04 17:13:33 +0100

    (strain.go) mentored

--- strain/strain.go
+++ strain/strain.go
@@ -1,14 +1,23 @@
 package strain
 
-func Discard[T any](list []T, predicate func(T) bool) (result []T) {
-	return Keep(list, func(item T) bool { return !predicate(item) })
-}
-
+// Keep returns a new slice containing only the elements from list
+// for which predicate returns true.
 func Keep[T any](list []T, predicate func(T) bool) (result []T) {
 	for _, item := range list {
 		if predicate(item) {
 			result = append(result, item)
 		}
 	}
-	return
+	return result
+}
+
+// Keep returns a new slice containing only the elements from list
+// for which predicate returns true.
+func Discard[T any](list []T, predicate func(T) bool) (result []T) {
+	for _, item := range list {
+		if !predicate(item) {
+			result = append(result, item)
+		}
+	}
+	return result
 }
