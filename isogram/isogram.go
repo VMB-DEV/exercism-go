8529fcadf77df24a3627f1d46b8e9eb61d702537
VMB-DEV <vkdev7360@gmail.com>
2026-01-08 09:14:10 +0100

    (isogram.go)

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -5,12 +5,13 @@ import "strings"
 func IsIsogram(word string) bool {
 	registeredChars := make(map[int32]struct{})
 	for _, char := range strings.ToLower(word) {
-		if _, exists := registeredChars[char]; !exists {
-			if char != int32('-') && char != int32(' ') {
+		countThisChar := char != '-' && char != ' '
+		if countThisChar {
+			if _, exists := registeredChars[char]; !exists {
 				registeredChars[char] = struct{}{}
+			} else {
+				return false
 			}
-		} else {
-			return false
 		}
 	}
 	return true
