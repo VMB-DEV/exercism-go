be56f64feb5a81eeaab717123f7a13881c8fcc68
VMB-DEV <vkdev7360@gmail.com>
2026-01-08 09:10:23 +0100

    (isogram.go)

---
+++ isogram/isogram.go
@@ -0,0 +1,18 @@
+package isogram
+
+import "strings"
+
+func IsIsogram(word string) bool {
+	//registeredChars := make(map[int32]struct{})
+	registeredChars := make(map[int32]struct{})
+	for _, char := range strings.ToLower(word) {
+		if _, exists := registeredChars[char]; !exists {
+			if char != int32('-') && char != int32(' ') {
+				registeredChars[char] = struct{}{}
+			}
+		} else {
+			return false
+		}
+	}
+	return true
+}
