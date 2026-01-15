044b3b863bad707ed8c42930ab12de79655e53bd
VMB-DEV <vkdev7360@gmail.com>
2026-01-15 09:23:04 +0100

    (resistor_color_trio.go)

--- resistor-color-trio/resistor_color_trio.go
+++ resistor-color-trio/resistor_color_trio.go
@@ -1,49 +1,45 @@
 package resistorcolortrio
 
 import (
-	"errors"
 	"fmt"
-	"strings"
+	"math"
 )
 
 // Label describes the resistance value given the colors of a resistor.
 // The label is a string with a resistance value with an unit appended
 // (e.g. "33 ohms", "470 kiloohms").
 
-var colorData = map[string][]string{
-	"black":  {"0", "", " ohms"},
-	"brown":  {"1", "1", "0 ohms"},
-	"red":    {"2", "2", " kiloohms"},
-	"orange": {"3", "3", " kiloohms"},
-	"yellow": {"4", "4", "0 kiloohms"},
-	"green":  {"5", "5", " megaohms"},
-	"blue":   {"6", "6", " megaohms"},
-	"violet": {"7", "7", " gigaohms"},
-	"grey":   {"8", "8", " gigaohms"},
-	"white":  {"9", "9", " gigaohms"},
-}
-
-// ColorCode returns the resistance value of the given color.
-func ColorData(color string) ([]string, error) {
-	if data, exists := colorData[color]; exists {
-		return data, nil
-	} else {
-		return data, errors.New(fmt.Sprintf("Color %s does not exist", color))
-	}
+var colorCode = map[string]int{
+	"black":  0,
+	"brown":  1,
+	"red":    2,
+	"orange": 3,
+	"yellow": 4,
+	"green":  5,
+	"blue":   6,
+	"violet": 7,
+	"grey":   8,
+	"white":  9,
 }
 
 func Label(colors []string) string {
-	var elements []string
-	for i, color := range colors[:3] {
-		data, err := ColorData(color)
-		if err != nil || i >= len(data) {
+	if len(colors) < 3 {
+		return ""
+	}
+	for _, color := range colors[:3] {
+		if _, exist := colorCode[color]; !exist {
 			return ""
 		}
-		elements = append(elements, data[i])
 	}
-	value := strings.Join(elements, "")
-	if value[0] == '0' && value != "0 ohms" {
-		value = value[1:]
+	number := (colorCode[colors[0]]*10 + colorCode[colors[1]]) * int(math.Pow10(colorCode[colors[2]]))
+	switch {
+	case number >= 1_000_000_000:
+		return fmt.Sprintf("%d gigaohms", number/1_000_000_000)
+	case number >= 1_000_000:
+		return fmt.Sprintf("%d megaohms", number/1_000_000)
+	case number >= 1_000:
+		return fmt.Sprintf("%d kiloohms", number/1_000)
+	default:
+		return fmt.Sprintf("%d ohms", number)
 	}
-	return value
 }
